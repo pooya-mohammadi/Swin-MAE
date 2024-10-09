ce3388dc321b20aa987ae956385957cb52b028c5
pooya-mohammadi <pooyamohammadikazaj@gmail.com>
2024-10-09 16:10:17 +0200

    Update remove_no_seg_images.py

--- remove_no_seg_images.py
+++ remove_no_seg_images.py
@@ -2,6 +2,8 @@ import os
 import shutil
 from argparse import ArgumentParser
 from os.path import join, split
+
+import PIL.Image
 import numpy as np
 from deep_utils import NIBUtils, DirUtils
 from PIL import Image
@@ -12,7 +14,9 @@ from deep_utils import NIBUtils
 parser = ArgumentParser()
 parser.add_argument("--input_img", default="/home/aicvi/projects/Swin-MAE-datasets/images/ct_coronary/")
 parser.add_argument("--input_seg", default="/home/aicvi/projects/Swin-MAE-datasets/labels/ct_coronary/")
-parser.add_argument("--remove_empty", action="store_true", default="If set will remove segments without target")
+parser.add_argument("--img_ext", default=".jpg", type=str)
+parser.add_argument("--seg_ext", default=".jpg", type=str)
+parser.add_argument("--remove_empty", action="store_true", help="If set will remove segments without target")
 
 args = parser.parse_args()
 
@@ -29,8 +33,8 @@ def check_seg(seg_files: dict[str, str]) -> dict[str, str]:
 
 
 if __name__ == '__main__':
-    images = DirUtils.list_dir_full_path(args.input_img, interest_extensions=".jpg", return_dict=True)
-    labels = DirUtils.list_dir_full_path(args.input_seg, interest_extensions=".npz", return_dict=True)
+    images = DirUtils.list_dir_full_path(args.input_img, interest_extensions=args.img_ext, return_dict=True)
+    labels = DirUtils.list_dir_full_path(args.input_seg, interest_extensions=args.seg_ext, return_dict=True)
 
     if args.remove_empty:
         labels = check_seg(labels)
@@ -41,8 +45,8 @@ if __name__ == '__main__':
             os.remove(i_v)
             n += 1
 
-    images = DirUtils.list_dir_full_path(args.input_img, interest_extensions=".jpg", return_dict=True)
-    labels = DirUtils.list_dir_full_path(args.input_seg, interest_extensions=".npz", return_dict=True)
+    images = DirUtils.list_dir_full_path(args.input_img, interest_extensions=args.img_ext, return_dict=True)
+    labels = DirUtils.list_dir_full_path(args.input_seg, interest_extensions=args.seg_ext, return_dict=True)
     print(f"Removed {n} empty samples")
     print(len(labels), len(images))
 
@@ -50,15 +54,15 @@ if __name__ == '__main__':
     n = 0
     for img_key, img_value in tqdm(images.items(), desc="Removing non consistent shapes"):
         label_value = labels[img_key]
-        label = np.load(label_value)['arr_0']
+        label = np.load(label_value)['arr_0'] if label_value.endswith(".npz") else np.array(PIL.Image.open(label_value))
         image = np.array(Image.open(img_value))[:, :, 2]
         if image.shape != label.shape:
             os.remove(img_value)
             os.remove(label_value)
             n += 1
 
-    images = DirUtils.list_dir_full_path(args.input_img, interest_extensions=".jpg", return_dict=True)
-    labels = DirUtils.list_dir_full_path(args.input_seg, interest_extensions=".npz", return_dict=True)
+    images = DirUtils.list_dir_full_path(args.input_img, interest_extensions=args.img_ext, return_dict=True)
+    labels = DirUtils.list_dir_full_path(args.input_seg, interest_extensions=args.seg_ext, return_dict=True)
     print(f"Removed {n} samples")
     print(len(labels), len(images))
 
